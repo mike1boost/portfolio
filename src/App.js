1882b34c0b4843ae81e13cf3cecfec8dd8672a31
mike1boost <sky2mike1@gmail.com>
2019-04-03 21:00:15 +0300

    add buttons read more and less

--- src/App.js
+++ src/App.js
@@ -4,23 +4,53 @@ class App extends Component{
     constructor(){
         super();
         this.state = {
-            dispalyBio: false,
+            displayBio: false,
         }
     }
+
+    toggleDisplayBio = () => {
+        this.setState((prevState) => {
+           return {displayBio: !prevState.displayBio}; 
+        }); 
+    };
+
+    showof = ()=>{
+        
+        let list = [];
+        for(let i =0; i<3; i++){
+            list.push(
+                     <div>
+                         <p>hey {i}</p>
+                         <p>Michael is the best {i}</p>
+                      </div>
+                     );            
+        }
+        return list;    
+    }
+
     render(){
+        // let showa = this.showof();
+        
+        
         return(
             <div>
                 <h1>Hello You</h1> 
                 <h3>Michael Fridman</h3>
+                {this.showof()}
                 {
-                    this.state.dispalyBio ? (
+                    this.state.displayBio ? (
                     <div>
                         <p>My favorite language is javascript</p>
                         <p>Beside code I love to travel across the world</p>
                         <p>World is changing </p>
                         <p>i'm happy to be programer </p>
+                        <button onClick={this.toggleDisplayBio}>Show Less</button>
                     </div>
-                    ) : null
+                    ) : (
+                    <div>
+                        <button onClick={this.toggleDisplayBio}>Read More</button>
+                    </div>
+                    )
                 }
             </div>
         )

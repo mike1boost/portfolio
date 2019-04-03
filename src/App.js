a04eb0a5d4936844bfaa0f3b0f1d80176c970d37
mike1boost <sky2mike1@gmail.com>
2019-04-03 16:46:30 +0300

    start adding more info about my self

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 
 class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            dispalyBio: false,
+        }
+    }
     render(){
         return(
-            <div>Hello my name is <h1>Michael Fridman</h1>
-                <p>i'm happy to be her</p>
+            <div>
+                <h1>Hello You</h1> 
+                <h3>Michael Fridman</h3>
+                {
+                    this.state.dispalyBio ? (
+                    <div>
+                        <p>My favorite language is javascript</p>
+                        <p>Beside code I love to travel across the world</p>
+                        <p>World is changing </p>
+                        <p>i'm happy to be programer </p>
+                    </div>
+                    ) : null
+                }
             </div>
         )
     };
